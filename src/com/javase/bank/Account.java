214f9f9ede68e2813720d38d137543c20ebf4e80
daixiongkun <514590399@qq.com>
2018-07-08 22:48:53 +0800

    修改方法

--- src/com/javase/bank/Account.java
+++ src/com/javase/bank/Account.java
@@ -1,4 +1,25 @@
 package com.javase.bank;
 
 public class Account {
+    private double balance;
+    public Account(double init_balance){
+        this.balance = init_balance;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public boolean deposit(double amt){
+        this.balance += amt;
+        return true;
+    }
+
+    public boolean withdraw(double amt){
+        if (this.balance<amt){
+            return false;
+        }
+        this.balance -= amt;
+        return true;
+    }
 }
--- src/com/javase/bank/Customer.java
+++ src/com/javase/bank/Customer.java
@@ -1,4 +1,28 @@
 package com.javase.bank;
 
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private Account account;
+
+    public Customer(String f,String l){
+        this.firstName = f;
+        this.lastName = l;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setAccount(){
+        this.account = account;
+    }
+
+    public Account getAccount(Account account){
+        return account;
+    }
 }
